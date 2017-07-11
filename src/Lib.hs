a4eadd45526c657c2ad9130e7e002549f9a134e4
Andrew Newman <andrewfnewman@gmail.com>
2017-07-11 11:18:52 +1000

    Compiles it works

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,7 +8,9 @@ import qualified Data.ByteString                 as BS
 import qualified Data.ByteString.Lazy            as LBS (ByteString, fromStrict,
                                                          readFile, writeFile)
 import           Data.Geography.GeoJSON          as GJ
+import           Data.HashMap.Strict             as HM
 import           Data.Map.Lazy                   as DMZ
+import           Data.Maybe
 import           Data.Monoid                     ((<>))
 import           Data.Scientific
 import           Data.Text                       (Text, pack)
@@ -39,18 +41,20 @@ zzz = do
     x <- readGeoJson "./test/integration/19781.json"
     pure (features x)
 
-aaa :: IO ([VT.Feature VG.Point], [VT.Feature VG.LineString], [VG.Polygon])
+aaa :: IO ([VT.Feature VG.Point], [VT.Feature VG.LineString], [VT.Feature VG.Polygon])
 aaa = do
     x <- zzz
     pure (foobar x)
 
-foobar :: [GJ.Feature] -> ([VT.Feature VG.Point], [VT.Feature VG.LineString], [VG.Polygon])
+foobar :: [GJ.Feature] -> ([VT.Feature VG.Point], [VT.Feature VG.LineString], [VT.Feature VG.Polygon])
 foobar = Prelude.foldr (\x (p, l, o) -> convertFeature p l o x) ([], [], [])
 
 convertFeature p l o (GJ.Feature bb (GJ.Point (GJ.PointGeometry c)) props id) = (VT.Feature 0 (convertProps props) (DV.fromList (moreTerrible c)) : p, l, o)
-convertFeature p l o (GJ.Feature bb (GJ.MultiPoint (GJ.MultiPointGeometry mpg)) props id) = (VT.Feature 0 DMZ.empty (DV.fromList (blerg [] mpg)) : p, l, o)
-convertFeature p l o (GJ.Feature bb (GJ.LineString ls) props id) = (p, VT.Feature 0 DMZ.empty (DV.fromList (blergLine [] [ls])) : l, o)
-convertFeature p l o (GJ.Feature bb (GJ.MultiLineString (GJ.MultiLineStringGeometry mls)) props id) = (p, VT.Feature 0 DMZ.empty (DV.fromList (blergLine [] mls)) : l, o)
+convertFeature p l o (GJ.Feature bb (GJ.MultiPoint (GJ.MultiPointGeometry mpg)) props id) = (VT.Feature 0 (convertProps props) (DV.fromList (blerg [] mpg)) : p, l, o)
+convertFeature p l o (GJ.Feature bb (GJ.LineString ls) props id) = (p, VT.Feature 0 (convertProps props) (DV.fromList (blergLine [] [ls])) : l, o)
+convertFeature p l o (GJ.Feature bb (GJ.MultiLineString (GJ.MultiLineStringGeometry mls)) props id) = (p, VT.Feature 0 (convertProps props) (DV.fromList (blergLine [] mls)) : l, o)
+convertFeature p l o (GJ.Feature bb (GJ.Polygon poly) props id) = (p, l, VT.Feature 0 (convertProps props) (DV.fromList (blergPoly [] [poly])) : o)
+convertFeature p l o (GJ.Feature bb (GJ.MultiPolygon (GJ.MultiPolygonGeometry polys)) props id) = (p, l, VT.Feature 0 (convertProps props) (DV.fromList (blergPoly [] polys)) : o)
 convertFeature p l o _ = (p, l, o)
 
 blerg :: [VG.Point] -> [GJ.PointGeometry] -> [VG.Point]
@@ -58,6 +62,9 @@ blerg = Prelude.foldr (\pg acc -> moreTerrible (coordinates pg) <> acc)
 
 blergLine :: [VG.LineString] -> [GJ.LineStringGeometry] -> [VG.LineString]
 blergLine = Prelude.foldr (\lsg acc -> VG.LineString (DVU.fromList (blerg [] (lineString lsg))) : acc)
+
+blergPoly :: [VG.Polygon] -> [GJ.PolygonGeometry] -> [VG.Polygon]
+blergPoly = Prelude.foldr (\poly acc -> VG.Polygon (DVU.fromList (blerg [] (exterior poly))) (DV.fromList (blergPoly [] (Prelude.fmap (\x -> GJ.PolygonGeometry x []) (holes poly)))) : acc)
 
 sToF :: Scientific -> Float
 sToF n = toRealFloat n :: Float
@@ -68,8 +75,13 @@ fToInt = round
 terrible = fToInt . sToF
 
 convertProps :: Value -> Map Text Val
-convertProps (Object x) = undefined
+convertProps (Object x) = DMZ.fromList $ catMaybes $ Prelude.fmap convertElems (HM.toList x)
 convertProps _ = DMZ.empty
+
+convertElems (k, String v) = Just (k, St v)
+convertElems (k, Number v) = Just (k, Fl (sToF v))
+convertElems (k, Bool v) = Just (k, B v)
+convertElems (k, v) = Nothing
 
 moreTerrible :: [Scientific] -> [VG.Point]
 moreTerrible [] = []
