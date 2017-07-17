55cfce0b1620d06943c20d7a5ba8b8be5cdf0f94
Andrew Newman <andrewfnewman@gmail.com>
2017-07-17 15:01:04 +1000

    Cleanup

--- src/Clip.hs
+++ src/Clip.hs
@@ -26,33 +26,33 @@ clipLines bb lines = undefined
 
 findOutcode bb lines = fmap evalDiffKeepSame <$> makeAPass bb lines
   where
-    evalDiffKeepSame (a@(o1, p1), b@(o2, p2)) =
+    evalDiffKeepSame ((o1, p1), (o2, p2)) =
       case compare o1 o2 of
         GT -> (clipPoint o1 bb p1 p2, p2)
         LT -> (p1, clipPoint o2 bb p1 p2)
         EQ -> (p1, p2)
 
 -- makeAPass :: (VG.Point, VG.Point) -> f VG.LineString -> f [((OutCode, t1), (OutCode, t))]
-makeAPass bb lines = foldr (\l acc -> keepInRemoveOut l ++ acc) [] <$> xxx bb lines
+makeAPass bb lines = filter removeOutside <$> outCodeForLines bb lines
   where
-    keepInRemoveOut (a@(o1, _), b@(o2, _)) =
+    removeOutside ((o1, _), (o2, _)) =
       case (o1, o2) of
-        (Clip.Left, Clip.Left) -> []
-        (Clip.Right, Clip.Right) -> []
-        (Bottom, Bottom) -> []
-        (Top, Top) -> []
-        _ -> [(a, b)]
+        (Clip.Left   , Clip.Left  ) -> False
+        (Clip.Right  , Clip.Right ) -> False
+        (Clip.Bottom , Clip.Bottom) -> False
+        (Clip.Top    , Clip.Top   ) -> False
+        _ -> True
 
 clipPoint outCode ((minx, miny), (maxx, maxy)) (x1, y1) (x2, y2) =
   case outCode of
-    Clip.Top -> (x1 + (x2 - x1) * (maxy - y1) `div` (y2 - y1), maxy)
-    Clip.Left -> (minx, y1 + (y2 - y1) * (minx - x1) `div` (x2 - x1))
-    Clip.Right -> (maxx, y1 + (y2 - y1) * (maxx - x1) `div` (x2 - x1))
+    Clip.Left   -> (minx, y1 + (y2 - y1) * (minx - x1) `div` (x2 - x1))
+    Clip.Right  -> (maxx, y1 + (y2 - y1) * (maxx - x1) `div` (x2 - x1))
     Clip.Bottom -> (x1 + (x2 - x1) * (miny - y1) `div` (y2 - y1), miny)
+    Clip.Top    -> (x1 + (x2 - x1) * (maxy - y1) `div` (y2 - y1), maxy)
     otherwise -> undefined
 
 -- xxx :: (Functor f) => (VG.Point, VG.Point) -> f VG.LineString -> f [[(OutCode, String)]]
-xxx bb = fmap (fmap (fmap (\(p1, p2) -> (toP1 bb p1, toP2 bb p2))) getLines)
+outCodeForLines bb = fmap (fmap (fmap (\(p1, p2) -> (toP1 bb p1, toP2 bb p2))) getLines)
   where
     getLines line = linesFromPoints . DVU.toList $ VG.lsPoints line
     toP1 bb p1 = (computeOutCode bb p1, p1)
@@ -67,10 +67,10 @@ table = [(Inside, 0), (Clip.Left, 1), (Clip.Right, 2), (Bottom, 4), (Top, 8)]
 
 computeOutCode :: (VG.Point, VG.Point) -> VG.Point -> OutCode
 computeOutCode ((minX, minY), (maxX, maxY)) (x,y)
-  | x < minX = Clip.Left
-  | x > maxX = Clip.Right
-  | y < minY = Clip.Top
-  | y > maxY = Clip.Bottom
+  | x < minX  = Clip.Left
+  | x > maxX  = Clip.Right
+  | y < minY  = Clip.Top
+  | y > maxY  = Clip.Bottom
   | otherwise = Clip.Inside
 
 clipPolygons :: [VG.Point] -> [VG.Polygon] -> [VG.Polygon]
@@ -131,10 +131,11 @@ poly = VG.Polygon (DVU.fromList polyPts) mempty
 polyPts = [( 50,150), (200, 50), (350,150), (350,300), (250,300),
            (200,250), (150,350), (100,250), (100,200)] :: [(Int,Int)]
 clipPts = [(100,100), (300,100), (300,300), (100,300)] :: [(Int,Int)]
-bb = ((10,10),(60,60)) :: ((Int, Int), (Int, Int))
-lines = [VG.LineString (DVU.fromList ([(11, 11), (59, 59)] :: [(Int,Int)])),
+linesBbTst = ((10,10),(60,60)) :: ((Int, Int), (Int, Int))
+linesTst = [VG.LineString (DVU.fromList ([(11, 11), (59, 59)] :: [(Int,Int)])),
   VG.LineString (DVU.fromList ([(0, 0), (0, 100)] :: [(Int,Int)])),
-  VG.LineString (DVU.fromList ([(0, 0), (70, 70)] :: [(Int,Int)]))]
+  VG.LineString (DVU.fromList ([(0, 0), (70, 70)] :: [(Int,Int)])),
+  VG.LineString (DVU.fromList ([(0, 0), (60, 60)] :: [(Int,Int)]))]
 answer = [(100,116),(124,100),(275,100),(300,116),(300,300),(250,300),(200,250),(175,300),(125,300),(100,250)]
 -- [{100 116.66667} {125 100} {275 100} {300 116.66667} {300 300} {250 300} {200 250}
 --  {175 300} {125 300} {100 250}]
