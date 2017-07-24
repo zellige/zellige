9b758ef416d112b779d42c0f1fcf4e9fb56332b0
Andrew Newman <andrewfnewman@gmail.com>
2017-07-24 16:20:19 +1000

    More tests

--- test/Data/Geometry/ClipSpec.hs
+++ test/Data/Geometry/ClipSpec.hs
@@ -2,6 +2,7 @@
 
 module Data.Geometry.ClipSpec where
 
+import qualified Data.Vector                   as DV
 import qualified Data.Vector.Unboxed           as DVU
 import qualified Geography.VectorTile.Geometry as VG
 import           Test.Hspec                    (Spec, describe, it, shouldBe)
@@ -12,37 +13,45 @@ poly :: VG.Polygon
 poly = VG.Polygon (DVU.fromList polyPts) mempty
 
 polyPts :: [(Int, Int)]
-polyPts = [( 50,150), (200, 50), (350,150), (350,300), (250,300),
-            (200,250), (150,350), (100,250), (100,200)] :: [(Int,Int)]
+polyPts = [(50,150), (200, 50), (350,150), (350,300), (250,300),
+            (200,250), (150,350), (100,250), (100,200)]
 
-clipPts :: ((Int, Int), (Int, Int))
-clipPts = ((100, 100), (300, 300))
+polyClipPts :: ((Int, Int), (Int, Int))
+polyClipPts = ((100, 100), (300, 300))
+
+linesTst :: DV.Vector VG.LineString
+linesTst = DV.fromList
+  [ VG.LineString (DVU.fromList [(11, 11), (59, 59)])
+  , VG.LineString (DVU.fromList [(0, 0), (0, 100)])
+  , VG.LineString (DVU.fromList [(5, 5), (45, 50), (90, 140)])
+  , VG.LineString (DVU.fromList [(0, 0), (60, 60)])]
+
+lineClipPts :: ((Int, Int), (Int, Int))
+lineClipPts = ((10,10),(60,60))
 
 spec :: Spec
-spec =
+spec = do
+  testClipLine
   testClipPolygon
 
--- testPoly = clipPolygons clipPts [poly]
--- testSingle = clipPolygon clipPts poly
--- isOkay = VG.Polygon (DVU.fromList answer) mempty == testSingle
--- poly = VG.Polygon (DVU.fromList polyPts) mempty
--- polyPts = [( 50,150), (200, 50), (350,150), (350,300), (250,300),
---            (200,250), (150,350), (100,250), (100,200)] :: [(Int,Int)]
--- clipPts = ((100, 100), (300, 300))
--- linesBbTst = ((10,10),(60,60)) :: ((Int, Int), (Int, Int))
--- linesTst = [VG.LineString (DVU.fromList ([(11, 11), (59, 59)] :: [(Int,Int)])),
---   VG.LineString (DVU.fromList ([(0, 0), (0, 100)] :: [(Int,Int)])),
---   VG.LineString (DVU.fromList ([(5, 5), (45, 50), (90, 140)] :: [(Int,Int)])),
---   VG.LineString (DVU.fromList ([(0, 0), (60, 60)] :: [(Int,Int)]))]
--- answer = [(100,116),(124,100),(275,100),(300,116),(300,300),(250,300),(200,250),(175,300),(125,300),(100,250)]
--- [{100 116.66667} {125 100} {275 100} {300 116.66667} {300 300} {250 300} {200 250}
---  {175 300} {125 300} {100 250}]
+testClipLine :: Spec
+testClipLine =
+  describe "simple line test" $
+    it "Returns clipped line" $ do
+      let actual = clipLines lineClipPts linesTst
+          resultLines = DV.fromList
+            [ VG.LineString (DVU.fromList [(11, 11), (59, 59)])
+            , VG.LineString (DVU.fromList [])
+            , VG.LineString (DVU.fromList [(10, 11), (45, 50), (50, 60)])
+            , VG.LineString (DVU.fromList [(10, 10), (60, 60)])
+            ]
+      actual `shouldBe` resultLines
 
 testClipPolygon :: Spec
 testClipPolygon =
   describe "simple polygon test" $
     it "Returns clipped polygon" $ do
-      let actual = clipPolygon clipPts poly
+      let actual = clipPolygon polyClipPts poly
           resultPts = [(100,116),(124,100),(275,100),(300,116),(300,300),(250,300),(200,250),(175,300),(125,300),(100,250)]
           result = VG.Polygon (DVU.fromList resultPts) mempty
       actual `shouldBe` result
