6b18ff3884bb02065c89f0a77b4c7a0e650ac458
ozmat <m.labatino@gmail.com>
2017-12-21 10:53:56 +1000

    Refactor

--- src/Data/Geometry/MapnikVectorTile.hs
+++ src/Data/Geometry/MapnikVectorTile.hs
@@ -4,11 +4,11 @@
 
 module Data.Geometry.MapnikVectorTile where
 
-import           Control.Monad.IO.Class
 import qualified Control.Monad.ST                as ST
 import           Data.Aeson
 import qualified Data.ByteString                 as B
-import qualified Data.ByteString.Lazy            as LBS (readFile)
+import qualified Data.ByteString.Char8           as BS
+import qualified Data.ByteString.Lazy            as LBS
 import qualified Data.Geography.GeoJSON          as GJ
 import           Data.Map                        as M
 import           Data.Monoid                     ((<>))
@@ -24,29 +24,51 @@ import           Data.Geometry.SphericalMercator
 import           Data.Geometry.Types.MvtFeatures
 import           Data.Geometry.Types.Types
 
+-- Command line
+
 writeLayer :: LayerConfig OG.Unwrapped -> IO ()
 writeLayer lc = do
-    let config = configFromLayerConfig lc
-    inputLayer <- readLayer (_layerInput lc) config
-    let outputLayer = VT.encode . VT.untile $ VVT.VectorTile (M.fromList [(_layerName lc, inputLayer)])
-    liftIO (B.writeFile (_layerOutput lc) outputLayer)
+    mvt <- geoJsonFileToMvt (_layerInput lc) (configFromLayerConfig lc)
+    B.writeFile (_layerOutput lc) (encodeMvt mvt)
 
-readLayer :: FilePath -> Config -> IO VT.Layer
-readLayer filePath config = do
-    geoJson <- liftIO $ readGeoJson filePath
+-- File lib
+
+geoJsonFileToMvt :: FilePath -> Config -> IO VT.VectorTile
+geoJsonFileToMvt filePath config = do
+    geoJson <- readGeoJson filePath
     createMvt config geoJson
 
-createMvt :: Config -> GJ.FeatureCollection -> IO VT.Layer
+readGeoJson :: FilePath -> IO GJ.FeatureCollection
+readGeoJson geoJsonFile = do
+    bs <- LBS.readFile geoJsonFile
+    let ebs         = eitherDecode' bs :: Either String GJ.FeatureCollection
+        decodeError = error . (("Unable to decode " <> geoJsonFile <> ": ") <>)
+    pure (either decodeError id ebs)
+
+readMvt :: FilePath -> IO VVT.VectorTile
+readMvt filePath = do
+    b <- B.readFile filePath
+    let db               = VT.decode b
+        rawDecodeError a = error (("Unable to read " <> filePath <> ": ") <> T.unpack a)
+        x                = either rawDecodeError id db
+        t                = VT.tile x
+    pure (either rawDecodeError id t)
+
+-- Lib
+
+encodeMvt :: VT.VectorTile -> BS.ByteString
+encodeMvt = VT.encode . VT.untile
+
+createMvt :: Config -> GJ.FeatureCollection -> IO VT.VectorTile
 createMvt Config{..} geoJson = do
-    let
+    let extentsBb       = (_extents, boundingBox _gtc)
+        clipBb          = createBoundingBoxPts _buffer _extents
         MvtFeatures{..} = ST.runST $ getFeatures extentsBb geoJson
-        cP = DV.foldl' (accNewGeom (clipPoints clipBb)) DV.empty mvtPoints
-        cL = DV.foldl' (accNewGeom (clipLines clipBb)) DV.empty mvtLines
-        cO = DV.foldl' (accNewGeom (clipPolygons clipBb)) DV.empty mvtPolygons
-    pure $ VT.Layer _version _name cP cL cO (_pixels _extents)
-    where
-        extentsBb = (_extents, boundingBox _gtc)
-        clipBb = createBoundingBoxPts _buffer _extents
+        cP              = DV.foldl' (accNewGeom (clipPoints clipBb)) DV.empty mvtPoints
+        cL              = DV.foldl' (accNewGeom (clipLines clipBb)) DV.empty mvtLines
+        cO              = DV.foldl' (accNewGeom (clipPolygons clipBb)) DV.empty mvtPolygons
+        layer           = VT.Layer _version _name cP cL cO (_pixels _extents)
+    pure . VT.VectorTile $ M.fromList [(_name, layer)]
 
 accNewGeom :: (DV.Vector a -> DV.Vector a) -> DV.Vector (VVT.Feature a) -> VVT.Feature a -> DV.Vector (VVT.Feature a)
 accNewGeom convF acc startGeom = if DV.null genClip then acc else DV.cons newGeom acc
@@ -56,19 +78,3 @@ accNewGeom convF acc startGeom = if DV.null genClip then acc else DV.cons newGeo
 
 getFeatures :: (Pixels, BoundingBox) -> GJ.FeatureCollection -> ST.ST s MvtFeatures
 getFeatures extentsBb = geoJsonFeaturesToMvtFeatures extentsBb . GJ.features
-
-readGeoJson :: FilePath -> IO GJ.FeatureCollection
-readGeoJson geoJsonFile = do
-    bs <- LBS.readFile geoJsonFile
-    let ebs = eitherDecode' bs :: Either String GJ.FeatureCollection
-        decodeError = error . (("Unable to decode " <> geoJsonFile <> ": ") <>)
-    pure (either decodeError id ebs)
-
-readMvt :: FilePath -> IO VVT.VectorTile
-readMvt filePath = do
-    b <- B.readFile filePath
-    let db = VT.decode b
-        rawDecodeError a = error (("Unable to read " <> filePath <> ": ") <> T.unpack a)
-        x = either rawDecodeError id db
-        t = VT.tile x
-    pure (either rawDecodeError id t)
