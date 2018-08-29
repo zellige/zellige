efd5b8952cb28426931495d1f93a3fad3ed7c342
newmana <andrewfnewman@gmail.com>
2018-08-29 11:00:47 +1000

    Try some inline pragmas

--- src/Data/Geometry/Clip/Internal/Line.hs
+++ src/Data/Geometry/Clip/Internal/Line.hs
@@ -12,13 +12,16 @@ checkValidLineString pts =
   if VectorStorable.length (segmentToLine pts) >= 2
     then Just (VectorTile.LineString (segmentToLine pts))
     else Nothing
+{-# INLINE checkValidLineString #-}
 
 getLines :: VectorTile.LineString -> VectorStorable.Vector TypesGeography.StorableLine
 getLines line = linesFromPoints $ VectorTile.lsPoints line
+{-# INLINE getLines #-}
 
 -- Create segments from points [1,2,3] becomes [(1,2),(2,3)]
 linesFromPoints :: VectorStorable.Vector VectorTile.Point -> VectorStorable.Vector TypesGeography.StorableLine
 linesFromPoints x = (VectorStorable.zipWith TypesGeography.StorableLine <*> VectorStorable.tail) (VectorStorable.convert x)
+{-# INLINE linesFromPoints #-}
 
 -- Remove duplicate points in segments [(1,2),(2,3)] becomes [1,2,3]
 segmentToLine :: VectorStorable.Vector VectorTile.Point -> VectorStorable.Vector VectorTile.Point
@@ -26,6 +29,8 @@ segmentToLine l = if VectorStorable.length l > 1 then VectorStorable.cons start 
   where
     start = VectorStorable.head l
     second = VectorStorable.ifilter (\i _ -> odd i)
+{-# INLINE segmentToLine #-}
 
 foldPointsToLine :: VectorStorable.Vector TypesGeography.StorableLine -> VectorStorable.Vector VectorTile.Point
 foldPointsToLine = VectorStorable.foldr (mappend . (\(TypesGeography.StorableLine p1 p2) -> VectorStorable.fromList [p1, p2])) mempty
+{-# INLINE foldPointsToLine #-}
