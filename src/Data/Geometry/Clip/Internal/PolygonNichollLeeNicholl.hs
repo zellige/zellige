429a5118ed3218d8e3002c2916f45d213fe7fb36
Laurie Nicol <l.nicol.9@gmail.com>
2018-09-18 15:27:15 +1000

    Cleanup

--- src/Data/Geometry/Clip/Internal/PolygonNichollLeeNicholl.hs
+++ src/Data/Geometry/Clip/Internal/PolygonNichollLeeNicholl.hs
@@ -16,41 +16,6 @@ import qualified Data.List.NonEmpty               as ListNonEmpty
 import qualified Data.Validation                  as Validation
 import qualified Data.Vector                      as Vector
 
--- import qualified Data.Geometry.Clip.Internal.LineNichollLeeNicholl as ClipLine
-
--- clipPolygonsNLN :: TypesGeography.BoundingBoxPts -> Vector.Vector VectorTile.Polygon -> Vector.Vector VectorTile.Polygon
--- clipPolygonsNLN bb = Vector.foldl' addPoly mempty
---   where
---     addPoly acc f =
---       case clipPolygonNLN bb f of
---         Nothing -> acc
---         Just x  -> Vector.cons x acc
-
--- clipPolygonNLN :: TypesGeography.BoundingBoxPts -> VectorTile.Polygon -> Maybe VectorTile.Polygon
--- clipPolygonNLN bb (VectorTile.Polygon polyPoints inner) =
---   case clipPolyPoints bb polyPoints of
---     Nothing -> Nothing
---     Just x  -> Just (VectorTile.Polygon x (clipPolygonsNLN bb inner))
-
--- clipPolyPoints :: TypesGeography.BoundingBoxPts -> Vector.Vector Geospatial.PointXY -> Maybe (Vector.Vector Geospatial.PointXY)
--- clipPolyPoints bb polyPoints = checkLength (VectorStorable.uniq newClippedPoly)
---   where
---     newClippedPoly = foo (TypesGeography.bboxPtsToBboxRect bb) polyPoints
---     checkLength newPoly =
---       if VectorStorable.length newPoly <= 2
---         then Nothing
---         else Just (closeIfNot newPoly)
-
--- closeIfNot :: Vector.Vector Geospatial.PointXY -> Vector.Vector Geospatial.PointXY
--- closeIfNot poly =
---   if lastPt /= firstPt
---     then VectorStorable.cons lastPt poly
---     else poly
---   where
---     lastPt = VectorStorable.last poly
---     firstPt = VectorStorable.head poly
-
-
 clipPolygonsNLN :: TypesGeography.BoundingBox -> Geospatial.GeoMultiPolygon -> Geospatial.GeoFeature Aeson.Value -> Vector.Vector (Geospatial.GeoFeature Aeson.Value) -> Vector.Vector (Geospatial.GeoFeature Aeson.Value)
 clipPolygonsNLN bb (Geospatial.GeoMultiPolygon polys) (Geospatial.GeoFeature bbox _ props fId) acc =
   case maybeNewMultiPoly bb polys of
