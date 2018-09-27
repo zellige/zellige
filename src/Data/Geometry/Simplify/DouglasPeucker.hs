df73dcbbb9e4d599244d4672e2289d564322688f
Dominic Endrei <dominic.endrei@gmail.com>
2018-09-27 11:54:53 +1000

    Improve Douglas-Peucker

--- src/Data/Geometry/Simplify/DouglasPeucker.hs
+++ src/Data/Geometry/Simplify/DouglasPeucker.hs
@@ -10,6 +10,7 @@ import qualified Data.Geospatial               as Geospatial
 import qualified Data.Sequence                 as Sequence
 
 import qualified Data.Geometry.Types.Geography as TypesGeography
+import           Prelude                       hiding (last, tail)
 
 type Distance = Double
 type Index = Int
@@ -18,34 +19,22 @@ type Index = Int
 douglasPeucker :: Double -> Sequence.Seq Geospatial.PointXY -> Sequence.Seq Geospatial.PointXY
 douglasPeucker epsilon points
   | points == Sequence.empty = Sequence.empty
-  | dMax > epsilon = douglasPeucker epsilon left Sequence.>< seqTail (douglasPeucker epsilon right)
-  | otherwise = Sequence.fromList [(seqHead points), (seqLast points)]
+  | dMax > epsilon = douglasPeucker epsilon left Sequence.>< tail (douglasPeucker epsilon right)
+  | otherwise = firstAndLastPoint points
   where
     (left, right) = (Sequence.take index points, Sequence.drop (index - 1) points)
     (dMax, index) = splitAtMaxDistance points
 
 splitAtMaxDistance :: Sequence.Seq Geospatial.PointXY -> (Distance, Index)
-splitAtMaxDistance points =
-  Sequence.foldlWithIndex (\(accMax, index) ni a ->
-    if cp a ls > accMax
-      then (cp a ls, ni + 1)
+splitAtMaxDistance points@((first Sequence.:<| _) Sequence.:|> last) =
+  Sequence.foldlWithIndex (\(accMax, index) i point ->
+    if shortestDistance point lineSegment > accMax
+      then (shortestDistance point lineSegment, i + 1)
       else (accMax, index)
   ) (0.0, Sequence.length points) points
   where
-    ls = TypesGeography.GeoStorableLine (seqHead points) (seqLast points)
-    cp = shortestDistance
-
-seqHead :: Sequence.Seq a -> a
-seqHead s = Sequence.index s 0
-
-seqLast :: Sequence.Seq a -> a
-seqLast s = Sequence.index s ((Sequence.length s) - 1)
-
-seqTail :: Sequence.Seq a -> Sequence.Seq a
-seqTail s =
-  case Sequence.viewl s of
-    Sequence.EmptyL    -> Sequence.empty
-    (_ Sequence.:< xs) -> xs
+    lineSegment = TypesGeography.GeoStorableLine first last
+splitAtMaxDistance _ = (0.0, 0)
 
 -- http://paulbourke.net/geometry/pointlineplane/DistancePoint.java
 shortestDistance :: Geospatial.PointXY -> TypesGeography.GeoStorableLine -> Distance
@@ -61,3 +50,12 @@ shortestDistance p@(Geospatial.PointXY pX pY) (TypesGeography.GeoStorableLine a@
 distance :: Geospatial.PointXY -> Geospatial.PointXY -> Distance
 distance (Geospatial.PointXY x1 y1) (Geospatial.PointXY x2 y2) = sqrt(((x1 - x2) ** 2) + ((y1 - y2) ** 2))
 
+firstAndLastPoint :: Sequence.Seq Geospatial.PointXY -> Sequence.Seq Geospatial.PointXY
+firstAndLastPoint ((first Sequence.:<| _) Sequence.:|> last) = Sequence.fromList [first, last]
+firstAndLastPoint _ = Sequence.empty
+
+tail :: Sequence.Seq a -> Sequence.Seq a
+tail s =
+  case Sequence.viewl s of
+    Sequence.EmptyL    -> Sequence.empty
+    (_ Sequence.:< xs) -> xs
