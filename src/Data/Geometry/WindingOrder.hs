a663e25bab779c8686859b4459d5816ea206511f
Andrew Newman <andrewfnewman@gmail.com>
2020-03-25 14:37:24 +1000

    Move stuff about

---
+++ src/Data/Geometry/WindingOrder.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module Data.Geometry.WindingOrder (
+  surveyor
+, isClockwise
+, rewind
+, ensureOrder
+, WindingOrder(..)
+) where
+
+import qualified Data.Foldable   as Foldable
+import qualified Data.Geospatial as Geospatial
+import qualified Data.Sequence   as Sequence
+
+data WindingOrder = Clockwise | AntiClockwise
+
+-- Surveyor's/Shoelace formula https://en.wikipedia.org/wiki/Shoelace_formula
+-- https://stackoverflow.com/questions/451426/how-do-i-calculate-the-area-of-a-2d-polygon
+-- http://geomalgorithms.com/a01-_area.html - area2D_Polygon()
+-- https://rosettacode.org/wiki/Shoelace_formula_for_polygonal_area#Haskell
+-- [(3, 4), (5, 11), (12, 8), (9, 5), (5, 6), (3,4)] -> -30
+surveyor :: Sequence.Seq Geospatial.PointXY -> Double
+surveyor v = (/ 2) . Foldable.foldl' (+) 0 $ Sequence.zipWith3 (\xn yn yp -> xn * (yn - yp)) xs yns yps
+  where
+    -- [3,5,12,9,5,3]
+    xs = fmap Geospatial._xyX v
+    -- [4,11,8,5,6,4] -> [11,8,5,6,4,4]
+    yns = case v of
+            (hV' Sequence.:<| restV') -> fmap Geospatial._xyY (restV' Sequence.|> hV')
+            _ -> Sequence.empty
+    -- [4,11,8,5,6,4] -> [4,4,11,8,5,6]
+    yps = case v of
+            (restV' Sequence.:|> hV') -> fmap Geospatial._xyY (hV' Sequence.<| restV')
+            _ -> Sequence.empty
+{-# INLINE surveyor #-}
+
+isClockwise :: Sequence.Seq Geospatial.PointXY -> Bool
+isClockwise v = surveyor v <= 0
+
+rewind :: Sequence.Seq Geospatial.PointXY -> Sequence.Seq Geospatial.PointXY
+rewind v =
+    case v of
+      (firstPt Sequence.:<| (middle Sequence.:|> lastPt)) -> (firstPt Sequence.<| Sequence.reverse middle) Sequence.|> lastPt
+      _                                                   -> v
+
+ensureOrder :: WindingOrder -> Sequence.Seq Geospatial.PointXY ->  Sequence.Seq Geospatial.PointXY
+ensureOrder order v =
+  case order of
+    Clockwise     -> if isClockwise v then v else rewind v
+    AntiClockwise -> if isClockwise v then rewind v else v
+
--- test/Data/Geometry/PolygonSpec.hs
+++ test/Data/Geometry/PolygonSpec.hs
@@ -10,53 +10,17 @@ import qualified Data.Geometry.Clip.Internal.Polygon as InternalPolygon
 import qualified Data.SpecHelper                     as SpecHelper
 
 simplePolygonPts :: [(Double, Double)]
-simplePolygonPts = [(0, 0), (4, 0), (4, 4), (0, 4), (0, 0)]
+simplePolygonPts = [(0, 0), (4, 0), (4, 4), (0, 4)]
 
-smallNegativePolyPts :: [(Double, Double)]
-smallNegativePolyPts = [(3, 4), (5, 11), (12, 8), (9, 5), (5, 6), (3, 4)]
-
-largePositivePolyPts :: [(Double, Double)]
-largePositivePolyPts = [(3186, 2048), (3186, 2037), (3197, 2037), (3197, 2048), (3186, 2048)]
-
-rewoundSimplePolygonPts :: [(Double, Double)]
-rewoundSimplePolygonPts = [(0, 0), (0, 4), (4, 4), (4, 0), (0,0)]
-
-rewoundSmallNegativePolyPts :: [(Double, Double)]
-rewoundSmallNegativePolyPts = [(3, 4), (5, 6), (9, 5), (12, 8), (5, 11), (3, 4)]
-
-rewoundLargePositivePolyPts :: [(Double, Double)]
-rewoundLargePositivePolyPts = [(3186, 2048), (3197, 2048), (3197, 2037), (3186, 2037), (3186, 2048)]
+expectedSimplePolygonPts :: [(Double, Double)]
+expectedSimplePolygonPts = [(0, 0), (4, 0), (4, 4), (0, 4), (0,0)]
 
 spec :: Spec
 spec =
-  testShoelace
+  testCloseIfNot
 
-testShoelace :: Spec
-testShoelace = do
-  describe "areas" $ do
-    it "Small set of points 2" $
-      InternalPolygon.surveyor (SpecHelper.listToSequenceGeo simplePolygonPts) `shouldBe` 16
+testCloseIfNot :: Spec
+testCloseIfNot =
+  describe "close" $
     it "Small set of points" $
-      InternalPolygon.surveyor (SpecHelper.listToSequenceGeo smallNegativePolyPts) `shouldBe` (-30)
-    it "Large set of points" $
-      InternalPolygon.surveyor (SpecHelper.listToSequenceGeo largePositivePolyPts) `shouldBe` 121
-  describe "winding order" $ do
-    it "Small set of points 2" $
-      InternalPolygon.isClockwise (SpecHelper.listToSequenceGeo simplePolygonPts) `shouldBe` False
-    it "Small set of points" $
-      InternalPolygon.isClockwise (SpecHelper.listToSequenceGeo smallNegativePolyPts) `shouldBe` True
-    it "Large set of points" $
-      InternalPolygon.isClockwise (SpecHelper.listToSequenceGeo largePositivePolyPts) `shouldBe` False
-  describe "rewind" $ do
-    it "Small set of points 2" $
-      InternalPolygon.rewind (SpecHelper.listToSequenceGeo simplePolygonPts) `shouldBe` SpecHelper.listToSequenceGeo rewoundSimplePolygonPts
-    it "Small set of points" $
-      InternalPolygon.rewind (SpecHelper.listToSequenceGeo smallNegativePolyPts) `shouldBe` SpecHelper.listToSequenceGeo rewoundSmallNegativePolyPts
-    it "Large set of points" $
-      InternalPolygon.rewind (SpecHelper.listToSequenceGeo largePositivePolyPts) `shouldBe` SpecHelper.listToSequenceGeo rewoundLargePositivePolyPts
-  describe "ensure order" $ do
-    it "Small set of points 2" $
-      InternalPolygon.ensureOrder InternalPolygon.Clockwise (SpecHelper.listToSequenceGeo simplePolygonPts) `shouldBe` SpecHelper.listToSequenceGeo rewoundSimplePolygonPts
-    it "Large set of points" $
-      InternalPolygon.ensureOrder InternalPolygon.AntiClockwise (SpecHelper.listToSequenceGeo largePositivePolyPts) `shouldBe` SpecHelper.listToSequenceGeo largePositivePolyPts
-
+      InternalPolygon.closeIfNot (SpecHelper.listToSequenceGeo simplePolygonPts) `shouldBe` Just (SpecHelper.listToSequenceGeo expectedSimplePolygonPts)
